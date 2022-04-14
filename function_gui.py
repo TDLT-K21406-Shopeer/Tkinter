7dd5c2860ccac3e24aab3a7bc3b34a2998f0788c
Nguyễn Duy Duẩn <94855872+nguyenduyduan@users.noreply.github.com>
2022-04-14 20:04:16 +0700

    update

--- function_gui.py
+++ function_gui.py
@@ -1,22 +1,17 @@
 #Import các thư viện
 from tkinter import *
 import tkinter.messagebox as mess
-import smtplib
-from tkinter.scrolledtext import ScrolledText
-from tracemalloc import Statistic
-from turtle import st
 import data_gui as d
 
 # Tạo giao diện
 class Root(Tk):
-    def __init__(self,*args,**kwargs):
-        Tk.__init__(self,*args,**kwargs)
+    def __init__(self):
+        Tk.__init__(self)
         self.title("BMI's new version")
         self.iconbitmap(".\Picture\logo.ico")
-        # self.geometry("960x540")
         container = Frame(self)
         container.pack(side="top", fill="both", expand=True)
-        # self.resizable(False,False)
+        self.resizable(False,False)
 
         self.frames ={}
         for F in (StartPage, PageResult):
@@ -54,9 +49,10 @@ class StartPage(Frame):
         self.img2 = PhotoImage(file=".\Picture\woman.png")
         Label(self,image=self.img2).grid(row=1,column=5)
         Label(self,text="      ").grid(row=1,column=6)
+        
         # Tuổi
         self.v13=IntVar()
-        self.v13.set(18)
+        self.v13.set(0)
         self.age = Spinbox(self, from_= 2, width=29, to=120, font=d.font_button, textvariable=self.v13)
         self.age.grid(row=2,column=1,columnspan=6)
         Label(self,text="  Độ tuổi:",font=d.font_label).grid(sticky=W,row=2,column=0)
@@ -83,16 +79,20 @@ class StartPage(Frame):
     def check(self):
         try:
             age = int(self.v13.get())
-            if age>=2 and age<=120:
+            if age>=7 and age<=120:
                 try:
                     height = float(self.v2.get())
-                    if height<0:
+                    if height<=0:
                         mess.showerror("Lỗi","Chiều cao là một số dương")
+                    elif height>251:
+                        mess.showerror("Lỗi","Kỷ lục chiều cao trên thế giới là 251 cm")
                     else:
                         try:
                             weight = float(self.v3.get())
-                            if weight<0:
+                            if weight<=0:
                                 mess.showerror("Lỗi","Cân nặng là một số dương")
+                            elif weight>595:
+                                mess.showerror("Lỗi","Kỷ lục về cân nặng trên thế giới là 595 kg")
                             else:
                                 # Ghi kết quả vào file txt
                                 temp = open("temp.txt","w",encoding="utf-8")
@@ -101,18 +101,18 @@ class StartPage(Frame):
                                 temp.write(f"{height}\n")
                                 temp.write(f"{weight}")
                                 temp.close()
-                                mess.showinfo("Thông báo","Bạn đã nhập xong dữ liệu, hãy chọn vào Kết quả")
+                                mess.showinfo("Thông báo","Bạn đã nhập xong dữ liệu, hãy chọn vào Tiếp tục")
                                 self.result['state'] = NORMAL
                         except:
                             mess.showerror("Lỗi","Cân nặng là một số dương")
                 except:
                     mess.showerror("Lỗi","Chiều cao là một số dương")
-            elif age<2 and age>0:
+            elif age<7 and age>0:
                 mess.showerror("Lỗi","Công thức chỉ nên áp dụng cho trẻ từ 7 tuổi trở lên")
             elif age<0:
                 mess.showerror("Lỗi","Tuổi là một số dương")
             else:
-                mess.showerror("Lỗi","Kỷ lục thế giới về tuổi là 120") 
+                mess.showerror("Lỗi","Kỷ lục thế giới về tuổi là 120 tuổi") 
         except:
             mess.showerror("Lỗi","Tuổi là một số nguyên dương")
 
@@ -124,11 +124,12 @@ class PageResult(Frame):
 
         self.label_first = Label(self, text="Đây là chỉ số BMI của bạn", font="Courier 18").grid(row=0, column=0, columnspan=3)
         Label(self, text="   BMI:", font=d.font_label).grid(row=1, column=0, sticky=W)
+
         self.str_bmi=StringVar()
         self.str_state=StringVar()
         Label(self,text="   Đánh giá:",font=d.font_label).grid(row=2,column=0,sticky=W)
         Button(self, text="Kết quả", fg=d.color_fg, bg=d.color_bg, font=d.font_label, command=self.show_result).grid(row=0,column=4)
-        Button(self, text="Xóa kết quả", fg=d.color_fg, bg=d.color_bg, font=d.font_label, command=self.reset).grid(row=4,column=3,columnspan=2)
+        Button(self, text="Xóa kết quả", fg=d.color_bg, bg=d.color_fg, font=d.font_label, command=self.reset).grid(row=4,column=3,columnspan=2)
         Button(self, text="Trở về", fg=d.color_fg, bg=d.color_bg, font=d.font_label, command=lambda: controller.show_frame("StartPage")).grid(row=4,column=2)
     
     def show_result(self):
@@ -139,7 +140,6 @@ class PageResult(Frame):
         for s in data_list:
             l_strip.append(s.strip())
         bmi=round(float(l_strip[3])/((float(l_strip[2])/100)**2),2)
-        weight_max=float()
         self.str_bmi.set(bmi)
         Label(self, text=self.str_bmi.get(), font=d.font_button).grid(row=1,column=1,columnspan=2,pady=5,sticky=W)
         # state="Loading"
@@ -214,7 +214,7 @@ class MyMenu(Frame):
         settingMenu = Menu(mbar)    
         submenu = Menu(settingMenu)
         submenu.add_command(label="Thông tin", command=self.infor)
-        submenu.add_command(label="Mail")
+        submenu.add_command(label="Mail",command=self.show_mail)
         submenu.add_command(label="Phiên bản", command=self.ver)
         settingMenu.add_cascade(label="Chi tiết", menu=submenu)
         settingMenu.add_command(label="Thoát", command=self.quit)
